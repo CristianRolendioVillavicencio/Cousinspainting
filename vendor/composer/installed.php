155c0721af9a6aba895bd93fc015b6175c078990
Cristian Villavicencio <35142311+CristianRolendioVillavicencio@users.noreply.github.com>
2024-05-30 16:33:21 -0400

    git

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'cristianrolendiovillavicencio/cousinspainting',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '050889c398d3fd7e7e1c4feb32b722e6f87081d9',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'cristianrolendiovillavicencio/cousinspainting' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '050889c398d3fd7e7e1c4feb32b722e6f87081d9',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
